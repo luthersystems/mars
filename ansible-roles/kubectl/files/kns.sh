8c536952bdf649cf11609cd6d14dffd8883b3157
bmatsuo-at-luthersystems <bryan.matsuo@luthersystems.com>
2019-11-22 13:21:31 -0800

    ACRE-785:  Import eks_cluster_init, k8s_static_resources, and kubectl from ics-infrastructure
    Added bash profile script to the kubectl role to make convenience
    functions `setkns` and `kns` available on the bastion.

---
+++ ansible-roles/kubectl/files/kns.sh
@@ -0,0 +1,17 @@
+KUBECTL_NAMESPACE=default
+
+setkns() {
+    if [ -z "$1" ]; then
+        echo 'no namespace provided' 1>&2
+        return 1
+    fi
+    KUBECTL_NAMESPACE="$1"
+}
+
+kns() {
+    if [[ $# -eq 0 ]]; then
+        echo KUBECTL_NAMESPACE="$KUBECTL_NAMESPACE"
+        return 0
+    fi
+    kubectl -n "$KUBECTL_NAMESPACE" $@
+}
