f3ef27c4a940fc5177aa01a5aca1b5785485f467
Jeremy Asher <jeremy.asher@luthersystems.com>
2020-01-31 11:49:21 -0800

    ansible start-at-task
    This exposes the ansible-playbook argument `--start-at-task`.  This allows
    skipping forward in a run to retry a failed task.  Note that the full name of
    the task is required, including the role name for example.

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-set -x
+
 if [ x"$USER_ID" != x"0" ]; then
 	echo "default:x:${USER_ID}:${GROUP_ID}:Default User:${HOME}:/usr/sbin/nologin" >> /etc/passwd
 fi
 pwconv
 grpconv
-CMD="PATH=$PATH /opt/mars/mars.py $@"
-su -s "/bin/bash" -c "${CMD}" default 
+q() {
+    echo -n "'"
+    echo -n "$1" | sed "s/'/'\"'\"'/"
+    echo -n "'"
+}
+quote_args() {
+    for arg in "$@"; do
+        q "$arg"
+        echo -n " "
+    done
+}
+CMD="PATH=$PATH /opt/mars/mars.py $(quote_args "$@")"
+su -s "/bin/bash" -c "${CMD}" default
