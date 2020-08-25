6edcbb997a24130d77e1b82003784ca43a9f0603
Jeremy Asher <jeremy.asher@luthersystems.com>
2020-08-25 15:28:12 -0700

    Create release version 0.22.0

--- common.mk
+++ common.mk
@@ -1,7 +1,7 @@
 PROJECT=mars
 PROJECT_PATH=bitbucket.org/luthersystems/${PROJECT}
 
-VERSION=0.22.0-SNAPSHOT
+VERSION=0.22.0
 
 AWS_REGION=eu-west-2
 ECR_HOST=967058059066.dkr.ecr.${AWS_REGION}.amazonaws.com
