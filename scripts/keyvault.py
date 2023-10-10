12d6141270caa774e5037e96e219fa532839b6af
Sam Wood <sam.wood@luthersystems.com>
2023-10-10 15:17:27 -0700

    Revert "Use azurecli (#17)" (#18)
    This reverts commit 16bc1af7a462d4d5f4ec2b35033a708131647475.

--- scripts/keyvault.py
+++ scripts/keyvault.py
@@ -1,8 +1,8 @@
 from azure.keyvault.secrets import SecretClient
-from azure.identity import AzureCliCredential
+from azure.identity import DefaultAzureCredential
 
 def get_secret(az_vault, az_vault_key):
     vault_uri = f'https://{az_vault}.vault.azure.net'
-    credential = AzureCliCredential()
+    credential = DefaultAzureCredential()
     client = SecretClient(vault_url=vault_uri, credential=credential)
     return client.get_secret(az_vault_key).value
