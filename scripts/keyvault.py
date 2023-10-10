16bc1af7a462d4d5f4ec2b35033a708131647475
Sam Wood <sam.wood@luthersystems.com>
2023-10-10 14:02:17 -0700

    Use azurecli (#17)

--- scripts/keyvault.py
+++ scripts/keyvault.py
@@ -1,8 +1,8 @@
 from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
+from azure.identity import AzureCliCredential
 
 def get_secret(az_vault, az_vault_key):
     vault_uri = f'https://{az_vault}.vault.azure.net'
-    credential = DefaultAzureCredential()
+    credential = AzureCliCredential()
     client = SecretClient(vault_url=vault_uri, credential=credential)
     return client.get_secret(az_vault_key).value
